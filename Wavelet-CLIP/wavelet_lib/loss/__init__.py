30c4f744636361028119aaa8ef2b912349e76e63
LegenDUST <dhnam0502@naver.com>
2026-02-10 05:49:16 +0000

    Added pyproject.toml / fix old-styled __init__

---
+++ Wavelet-CLIP/wavelet_lib/loss/__init__.py
@@ -0,0 +1,5 @@
+from ..metrics.registry import LOSSFUNC
+
+from .cross_entropy_loss import CrossEntropyLoss
+
+__all__ = ['LOSSFUNC', 'CrossEntropyLoss']
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from deepfaker-detection!")
+
+
+if __name__ == "__main__":
+    main()
