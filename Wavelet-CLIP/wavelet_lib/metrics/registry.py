4849f112e8e903a6162538e4c0f24e5cb58cfedf
LegenDUST <dhnam0502@naver.com>
2026-02-11 06:08:53 +0000

    Typing to registry

--- Wavelet-CLIP/wavelet_lib/metrics/registry.py
+++ Wavelet-CLIP/wavelet_lib/metrics/registry.py
@@ -1,17 +1,18 @@
+import torch.nn as nn
 class Registry(object):
     def __init__(self):
-        self.data = {}
+        self.data:dict[str, nn.Module] = {}
 
-    def register_module(self, module_name=None):
-        def _register(cls):
+    def register_module(self, module_name: str|None=None):
+        def _register(cls: nn.Module):
             name = module_name
-            if module_name is None:
-                name = cls.__name__
+            if name is None:
+                name = cls.__class__.__name__
             self.data[name] = cls
             return cls
         return _register
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str):
         return self.data[key]
 
 DETECTOR = Registry()
