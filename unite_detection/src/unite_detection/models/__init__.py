11e3a25dc2991ba2570b929124356482e415a663
남도현 <2020097010@hanyang.ac.kr>
2026-02-19 10:20:46 +0900

    Removed unite_detection to use submodule

--- unite_detection/src/unite_detection/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .positional_encoding import TemporalPositionalEncoding
-from .unite_model import UNITE
-from .vit_encoder import ViTEncoder
-
-__all__ = ["TemporalPositionalEncoding", "UNITE", "ViTEncoder"]
