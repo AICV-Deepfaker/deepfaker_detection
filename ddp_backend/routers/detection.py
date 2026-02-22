17504054598471b57abf7d1ca3901c2beb32fd8f
LegenDUST <dhnam0502@naver.com>
2026-02-22 12:27:47 +0900

    Test adition of crud to predict - not working

--- ddp_backend/routers/detection.py
+++ ddp_backend/routers/detection.py
@@ -1,13 +1,19 @@
 from typing import Annotated
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Depends
+from sqlalchemy.orm import Session
 
-from ddp_backend.schemas import (
+from ddp_backend.schemas.crud import FastReportCreate
+
+from ..schemas import (
     APIOutputDeep,
     APIOutputFast,
 )
-from ddp_backend.services.dependencies import detection_pipeline
-from ddp_backend.utils.file_handler import save_temp_file
+from ..schemas.report import STTScript
+from ..services.dependencies import detection_pipeline
+from ..utils.file_handler import save_temp_file
+from ..services import crud
+from ..core.database import get_db
 
 
 router = APIRouter(prefix="/prediction", tags=["prediction"])
@@ -16,9 +22,29 @@ router = APIRouter(prefix="/prediction", tags=["prediction"])
 @router.post(path="/fast")
 def predict_deepfake_fast(
     file: Annotated[UploadFile, File(...)],
+    db: Annotated[Session, Depends(get_db)]
 ) -> APIOutputFast:
     with save_temp_file(file) as temp_path:
-        return detection_pipeline.run_fast_mode(temp_path)
+        output = detection_pipeline.run_fast_mode(temp_path)
+        assert output.wavelet is not None and output.r_ppg is not None
+        crud.create_fast_report(db, FastReportCreate(
+            user_id=...,
+            result_id=...,
+            freq_result=output.wavelet.result,
+            freq_conf=output.wavelet.confidence_score,
+            freq_image=output.wavelet.visual_report,
+            rppg_result=output.r_ppg.result,
+            rppg_conf=output.r_ppg.confidence_score,
+            rppg_image=output.r_ppg.visual_report,
+            stt_keyword=output.stt.keywords,
+            stt_risk_level=output.stt.risk_level,
+            stt_script=STTScript(
+                risk_reason=output.stt.risk_reason,
+                transcript=output.stt.transcript,
+                search_results=output.stt.search_results,
+            )
+        ))
+        return output
 
 
 @router.post("/deep")
