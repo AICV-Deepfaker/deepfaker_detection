a88c11e5879d57f8a827d9dc1e49a11f48049d44
LegenDUST <dhnam0502@naver.com>
2026-02-25 10:57:17 +0900

    Update session to with statement

--- ddp_backend/core/database.py
+++ ddp_backend/core/database.py
@@ -1,4 +1,3 @@
-from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel, create_engine
 from sqlmodel.orm.session import Session
 from ddp_backend.core.config import settings
@@ -6,8 +5,6 @@ from ddp_backend.core.config import settings
 # DB 엔진 생성 (이제 확실하게 문자열 URL이 들어갑니다)
 engine = create_engine(settings.DATABASE_URL)
 
-# 세션(접속권) 생성 (데이터베이스와 소통)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=Session) #autocommit = 자동 저장, autoflush = 자동 변경 사항 저장
 
 # 테이블을 만들 때 상속받을 기본 클래스
 # Base = declarative_base() # JPA의 Entity
@@ -15,8 +12,5 @@ class Base(SQLModel): ...
 
 # 요청 단위로 DB 세션을 생성하고 종료하는 함수
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db # API 로직(router)에 세션 전달
-    finally:
-        db.close()
+    with Session(engine, autocommit=False, autoflush=False) as session:
+        yield session
