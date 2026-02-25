2eda1c5fefb2e085cd5a3855c9d1bab84413cc38
LegenDUST <dhnam0502@naver.com>
2026-02-25 13:48:48 +0900

    broker type fix

--- ddp_backend/core/tk_broker.py
+++ ddp_backend/core/tk_broker.py
@@ -1,8 +1,10 @@
+from uuim import UUID
+
 from taskiq_redis import RedisAsyncResultBackend, ListQueueBroker
 import taskiq_fastapi
 from .config import REDIS_URL
 
-result_backend = RedisAsyncResultBackend[int | None](REDIS_URL)
+result_backend = RedisAsyncResultBackend[UUID | None](REDIS_URL)
 broker = ListQueueBroker(REDIS_URL).with_result_backend(result_backend)
 
 taskiq_fastapi.init(broker, "ddp_backend.main:app")
