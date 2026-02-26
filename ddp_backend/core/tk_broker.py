a3f6cb8088d8b5b1a3adfd32d858970327edf13d
jiyun-kang12 <jiyun_kang@ewha.ac.kr>
2026-02-26 06:07:23 +0000

    conflict solved

--- ddp_backend/core/tk_broker.py
+++ ddp_backend/core/tk_broker.py
@@ -4,7 +4,10 @@ from taskiq_redis import RedisAsyncResultBackend, ListQueueBroker
 import taskiq_fastapi
 from .config import REDIS_URL
 
+from taskiq_redis import RedisStreamBroker
+
+REDIS_URL = "redis://127.0.0.1:6379/0"
 result_backend = RedisAsyncResultBackend[UUID | None](REDIS_URL)
-broker = ListQueueBroker(REDIS_URL).with_result_backend(result_backend)
+broker = RedisStreamBroker(REDIS_URL).with_result_backend(result_backend)
 
 taskiq_fastapi.init(broker, "ddp_backend.main:app")
